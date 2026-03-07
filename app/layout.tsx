6016e1876002aadcccc61a97a56e1393152aa9ba
goodjj <m241549@st.yamagata-u.ac.jp>
2026-03-07 17:29:16 +0900

    Add Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -33,7 +33,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<SpeedInsights /></body>
     </html>
   );
 }
